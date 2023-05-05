f3cec057b5b1d9285747a32c848277b1e3a14319
Mauricio Villatoro <mauripr26@gmail.com>
2023-05-04 21:11:45 -0600

    add .env.example

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ window.axios = axios
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 // window.axios.defaults.withCredentials = true
-window.axios.defaults.baseURL = 'http://localhost/api'
+window.axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
